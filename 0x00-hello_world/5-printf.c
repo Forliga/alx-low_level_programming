18666e290e6f782eb65250124148113c029bf489
Forliga <moyahabomoketla@gmail.com>
2023-09-07 10:01:47 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+nclude<stdio.h>
+/**
+ * main entry level
+ *
+ * Return always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is apiece of art,\n");
+	return (0);
+}
