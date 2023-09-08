284a0731140054f61a44c2a567a00208bc5270d8
Forliga <moyahabomoketla@gmail.com>
2023-09-08 11:55:18 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= '2'; c++)
+        putchar(c);
+	putchar('\n');	
+	return (0);
+
+}
