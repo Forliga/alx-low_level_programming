2194c7386b81e51b08c98e07636d80bf7b7515bb
Forliga <moyahabomoketla@gmail.com>
2023-09-08 11:59:15 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,9 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/* more headers goes there *
+* betty style doc for function main goes there 
+*/
+
 int main(void)
 
 {
