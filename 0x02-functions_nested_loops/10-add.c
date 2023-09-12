52ef64a3982fd68720e7a0286aaf6cc44065419d
Forliga <moyahabomoketla@gmail.com>
2023-09-12 11:54:05 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add 
+ *@c1: The first number to be computed
+ *@c2: The second number to be computed
+ * return: integer value
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}
